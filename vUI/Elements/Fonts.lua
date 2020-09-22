19e04222b8b0f25595566e5ed2381dc8c73f4a3d
Hydra <ryanandrews135@gmail.com>
2020-09-22 09:10:37 -0600

    Remove font override

--- vUI/Elements/Fonts.lua
+++ vUI/Elements/Fonts.lua
@@ -2,9 +2,9 @@ local vUI, GUI, Language, Assets, Settings = select(2, ...):get()
 
 local Fonts = vUI:NewModule("Fonts")
 
-_G.STANDARD_TEXT_FONT = Assets:GetFont("PT Sans")
+--[[_G.STANDARD_TEXT_FONT = Assets:GetFont("PT Sans")
 _G.UNIT_NAME_FONT = Assets:GetFont("PT Sans")
-_G.DAMAGE_TEXT_FONT = Assets:GetFont("PT Sans")
+_G.DAMAGE_TEXT_FONT = Assets:GetFont("PT Sans")]]
 
 function Fonts:Load()
 	local Font = Assets:GetFont(Settings["ui-widget-font"])
